07a5ea92f8ebde328b9b939f0042d03064814a12
Bruno Fernandes <brad25999@gmail.com>
2023-05-09 23:35:16 -0300

    improving the rides widget

---
+++ app/src/main/java/com/example/cesar/ifride/utils/Util.kt
@@ -0,0 +1,12 @@
+package com.example.cesar.ifride.utils
+
+import com.google.firebase.Timestamp
+
+class Util {
+
+    fun dateTimeFromTimestamp(timestamp: Timestamp) {
+        val tdate = timestamp.toDate()
+        
+    }
+
+}
